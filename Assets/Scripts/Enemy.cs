9ca8f8a9d9c51e6639fe0de30359a4a81cc4f8d4
A-Aron226 <aroeder345@gmail.com>
2024-09-23 12:47:11 -0500

    Used dot product to create enemy's line of sight

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,8 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform target;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,21 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 directionToTarget = (target.position - transform.position).normalized; //target position - player position; normalize to shorten length of distance (to only get direction)
+        Vector3 forwardDirection = transform.forward;
+
+        float dot = Vector3.Dot(directionToTarget, forwardDirection); //dot product is a decimal of the range from 1 (front) to 0 (sides) to -1 (back)
+
+        if (dot > 0.5f)
+        {
+            Debug.Log("Player Detected!");
+        }
+
+        if (dot < -0.5f)
+        {
+            Debug.Log("Lost sight of Player");
+        }
+
+        //Debug.Log(dot);
     }
 }
