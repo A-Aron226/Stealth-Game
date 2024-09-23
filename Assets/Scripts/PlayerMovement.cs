f27853612c4ab678eb3fafebabc51a8557424719
A-Aron226 <aroeder345@gmail.com>
2024-09-23 11:04:23 -0500

    added sneaking mechanic
    transferred movement and sneaking to new input system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -22,11 +23,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate() //Based on physics framerate (not always consistent)
     {
-        var newInput = GetCameraInput(input, Camera.main);
-
-        var newVelocity = new Vector3(newInput.x * speed * Time.fixedDeltaTime, rb.velocity.y, newInput.z * speed * Time.fixedDeltaTime); //Temp variable; sets movement and gravity based on camera position (newInput)
-
-        rb.velocity = newVelocity;
+        OnMovement();
     }
 
     Vector3 GetCameraInput(Vector2 input, Camera cam) //setting movement based on camera position
@@ -40,5 +37,28 @@ public class PlayerMovement : MonoBehaviour
         camForward.Normalize();
 
         return input.x * camRight + input.y * camForward; //multiplying input by the camera vector then adding both x and y
+    }
+
+    public void OnMovement() //movement
+    {
+        var newInput = GetCameraInput(input, Camera.main);
+
+        var newVelocity = new Vector3(newInput.x * speed * Time.fixedDeltaTime, rb.velocity.y, newInput.z * speed * Time.fixedDeltaTime); //Temp variable; sets movement and gravity based on camera position (newInput)
+
+        rb.velocity = newVelocity;
+    }
+
+    public void OnSneak() //sneak button
+    {
+        speed = speed / 2; //halves the set speed to simulate slow movement like the player is sneaking
+
+        Debug.Log("Sneaking");
+    }
+
+    public void OnStand() //stand button
+    {
+        speed = speed * 2; //multiplies speed by 2 to simulate the character 'standing up' back to normal speed
+
+        Debug.Log("Standing");
     }
 }
