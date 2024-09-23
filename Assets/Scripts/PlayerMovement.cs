a7c000c6ac8adb5e6af4afa9a6df1c092d096c8d
A-Aron226 <aroeder345@gmail.com>
2024-09-23 10:14:44 -0500

    set up movement based on camera position

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,8 +22,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate() //Based on physics framerate (not always consistent)
     {
-        var newVelocity = new Vector3(input.x * speed * Time.fixedDeltaTime, rb.velocity.y, input.y * speed * Time.fixedDeltaTime); //Temp variable; sets movement (input.x) and gravity (input.y)
+        var newInput = GetCameraInput(input, Camera.main);
+
+        var newVelocity = new Vector3(newInput.x * speed * Time.fixedDeltaTime, rb.velocity.y, newInput.z * speed * Time.fixedDeltaTime); //Temp variable; sets movement and gravity based on camera position (newInput)
 
         rb.velocity = newVelocity;
+    }
+
+    Vector3 GetCameraInput(Vector2 input, Camera cam) //setting movement based on camera position
+    {
+        Vector3 camRight = cam.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 camForward = cam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        return input.x * camRight + input.y * camForward; //multiplying input by the camera vector then adding both x and y
     }
 }
